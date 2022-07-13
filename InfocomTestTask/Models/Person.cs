b1a62c9dbc8f86112041652f199458c08ad302ce
Torogeldi Tologonov <ttorogeldi@gmail.com>
2022-07-13 23:37:23 +0600

    Add age field

--- InfocomTestTask/Models/Person.cs
+++ InfocomTestTask/Models/Person.cs
@@ -14,13 +14,11 @@ namespace InfocomTestTask.Models
         [MaxLength(50, ErrorMessage = "Name is long 50")]
         public string LastName { get; set; }
 
+        [Required]
+        public int Age { get; set; }
         public Gender Sex { get; set; }
 
 
-        public string GetData()
-        {
-            return SureName;
-        }
     }
     public enum Gender : int
     {
